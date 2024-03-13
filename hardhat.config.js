fddbec4d9e9d728f0fc270f8209ee5e80893fb18
zZoMROT <zzomrot.ne@gmail.com>
2024-03-13 15:14:45 +0000

    Initial commit

---
+++ hardhat.config.js
@@ -0,0 +1,44 @@
+require('@nomicfoundation/hardhat-chai-matchers');
+require('@nomicfoundation/hardhat-ethers');
+require('@nomicfoundation/hardhat-verify');
+require('dotenv').config();
+require('hardhat-dependency-compiler');
+require('hardhat-deploy');
+require('hardhat-gas-reporter');
+require('hardhat-tracer');
+require('solidity-coverage');
+
+const { Networks, getNetwork } = require('@1inch/solidity-utils/hardhat-setup');
+
+const { networks, etherscan } = (new Networks()).registerAll();
+
+module.exports = {
+    solidity: {
+        compilers: [
+            {
+                version: '0.8.23',
+                settings: {
+                    optimizer: {
+                        enabled: true,
+                        runs: 1000000,
+                    },
+                    evmVersion: (networks[getNetwork()])?.hardfork || 'shanghai',
+                    viaIR: true,
+                },
+            },
+        ],
+    },
+    namedAccounts: {
+        deployer: {
+            default: 0,
+        },
+    },
+    networks,
+    etherscan,
+    tracer: {
+        enableAllOpcodes: true,
+    },
+    dependencyCompiler: {
+        paths: [],
+    },
+};
