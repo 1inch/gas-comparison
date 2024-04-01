2581fa54e3e4c22119ca5764422e38ea53e71e28
Denis <zzomrot.ne@gmail.com>
2024-04-01 17:34:09 +0100

    Fix limit orders with eth and add uniswap compare

--- test/LimitOrders.js
+++ test/LimitOrders.js
@@ -33,8 +33,6 @@ describe('LimitOrders', async function () {
             },
             WETH: await ethers.getContractAt('IWETH', '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2'),
             DAI: await ethers.getContractAt('IERC20', '0x6B175474E89094C44Da98b954EedeAC495271d0F'),
-            USDC: await ethers.getContractAt('IERC20', '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48'),
-            USDT: await ethers.getContractAt('IERC20', '0xdAC17F958D2ee523a2206206994597C13D831ec7'),
         };
 
         for (const wallet of [maker, taker]) {
@@ -57,18 +55,18 @@ describe('LimitOrders', async function () {
     }
 
     it('ETH => DAI', async function () {
-        const { maker, taker, tokens, inch, matcha } = await loadFixture(initContracts);
+        const { maker, taker, tokens, inch, matcha, uniswap } = await loadFixture(initContracts);
 
         const inchOrder = new InchOrder({
             makerAsset: await tokens.DAI.getAddress(),
-            takerAsset: await tokens.ETH.getAddress(),
+            takerAsset: await tokens.WETH.getAddress(),
             makingAmount: ether('0.1'),
             takingAmount: ether('0.01'),
             maker,
             verifyingContract: await inch.getAddress(),
         });
         const { r, vs } = await inchOrder.sign(maker);
-        const inchTx = await inch.connect(taker).fillOrder(inchOrder.order, r, vs, inchOrder.order.takingAmount, fillWithMakingAmount(inchOrder.order.makingAmount));
+        const inchTx = await inch.connect(taker).fillOrder(inchOrder.order, r, vs, inchOrder.order.makingAmount, fillWithMakingAmount(inchOrder.order.makingAmount), { value: inchOrder.order.takingAmount });
 
         const matchaOrder = new MatchaOrder({
             chainId: await getChainId(),
@@ -76,18 +74,32 @@ describe('LimitOrders', async function () {
             maker: maker.address,
             taker: taker.address,
             makerToken: await tokens.DAI.getAddress(),
-            takerToken: await tokens.ETH.getAddress(),
+            takerToken: await tokens.WETH.getAddress(),
             makerAmount: ether('0.1'),
             takerAmount: ether('0.01'),
         });
         const signature = await matchaOrder.sign(maker);
-        const matchaTx = await matcha.connect(taker).fillLimitOrder(matchaOrder.order, signature, matchaOrder.order.takerAmount);
+        const matchaTx = await matcha.connect(taker).fillLimitOrder(matchaOrder.order, signature, matchaOrder.order.takerAmount, { value: matchaOrder.order.takerAmount });
+
+        const uniswapOrder = new UniswapOrder({
+            chainId: await getChainId(),
+            verifyingContract: await uniswap.getAddress(),
+            deadline: Math.floor(Date.now() / 1000) + 1000,
+            maker,
+            nonce: await (new NonceManager(maker)).getNonce(),
+            inputTokenAddress: await tokens.DAI.getAddress(),
+            outputTokenAddress: await tokens.ETH.getAddress(),
+            inputAmount: ether('0.1'),
+            outputAmount: ether('0.01'),
+            permit2contractAddress: PERMIT2CONTRACT,
+        });
+        const signedOrder = await uniswapOrder.sign(maker);
+        const uniTx = await uniswap.connect(taker).execute(signedOrder, { value: uniswapOrder.order.info.outputs[0].startAmount });
 
         gasUsed['ETH => DAI'] = {
             inch: (await inchTx.wait()).gasUsed.toString(),
             matcha: (await matchaTx.wait()).gasUsed.toString(),
-            paraswap: 'not supported',
-            uniswap: 'not supported',
+            uniswap: (await uniTx.wait()).gasUsed.toString(),
         };
     });
 
