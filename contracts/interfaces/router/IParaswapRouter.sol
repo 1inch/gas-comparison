c0025d206e423a29fed0ea46da6405fd0a77ff26
Denis <zzomrot.ne@gmail.com>
2024-06-10 21:15:34 +0100

    Upgrade paraswap router

--- contracts/interfaces/router/IParaswapRouter.sol
+++ contracts/interfaces/router/IParaswapRouter.sol
@@ -1,6 +1,49 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.23;
 
+import { IERC20 } from "@openzeppelin/contracts/interfaces/IERC20.sol";
+
 interface IParaswapRouter {
     function swapOnUniswapV2Fork(address tokenIn, uint256 amountIn, uint256 amountOutMin, address weth, uint256[] memory pools) external;
+
+    /// @notice Struct for UniswapV2 swapExactAmountIn/swapExactAmountOut data
+    /// @param srcToken The token to swap from
+    /// @param destToken The token to swap to
+    /// @param fromAmount The amount of srcToken to swap
+    /// = amountIn for swapExactAmountIn and maxAmountIn for swapExactAmountOut
+    /// @param quotedAmount The quoted expected amount of destToken/srcToken
+    /// = quotedAmountOut for swapExactAmountIn and quotedAmountIn for swapExactAmountOut
+    /// @param toAmount The minimum amount of destToken to receive
+    /// = minAmountOut for swapExactAmountIn and amountOut for swapExactAmountOut
+    /// @param metadata Packed uuid and additional metadata
+    /// @param beneficiary The address to send the swapped tokens to
+    /// @param pools data consisting of concatenated token0 and token1 address for each pool with the direction flag being
+    /// the right most bit of the packed token0-token1 pair bytes used in the path
+    struct UniswapV2Data {
+        IERC20 srcToken;
+        IERC20 destToken;
+        uint256 fromAmount;
+        uint256 toAmount;
+        uint256 quotedAmount;
+        bytes32 metadata;
+        address payable beneficiary;
+        bytes pools;
+    }
+
+    /// @notice Executes a swapExactAmountIn on Uniswap V2 pools
+    /// @param uniData struct containing data for the swap
+    /// @param partnerAndFee packed partner address and fee percentage, the first 12 bytes is the feeData and the last
+    /// 20 bytes is the partner address
+    /// @param permit The permit data
+    /// @return receivedAmount The amount of destToken received after fees
+    /// @return paraswapShare The share of the fees for Paraswap
+    /// @return partnerShare The share of the fees for the partner
+    function swapExactAmountInOnUniswapV2(
+        UniswapV2Data calldata uniData,
+        uint256 partnerAndFee,
+        bytes calldata permit
+    )
+        external
+        payable
+        returns (uint256 receivedAmount, uint256 paraswapShare, uint256 partnerShare);
 }
