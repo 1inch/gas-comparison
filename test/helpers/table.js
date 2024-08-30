d961ad4a524e15d80bfc6335dd159dc50067ffa7
Denis <zzomrot.ne@gmail.com>
2024-08-30 09:33:27 +0100

    Add percentage diff in tables

--- test/helpers/table.js
+++ test/helpers/table.js
@@ -72,6 +72,14 @@ class GasUsedTable {
     }
 
     toString() {
+        return this.toStringWithPercentages();
+    }
+
+    toStringDefault() {
+        return this.table.toString();
+    }
+
+    toStringWithColors() {
         for (let i = 1; i < this.table.length; i++) {
             const row = this.table[i];
             const sortRow = row.slice(1).sort(this.compareElementsHook);
@@ -91,6 +99,49 @@ class GasUsedTable {
                 }
                 this.table[i][j] = chalk[cellColor](this.table[i][j]);
             }
+        }
+        return this.table.toString();
+    }
+
+    toStringWithPercentages() {
+        // Patch table lline with dex names
+        for (let i = 2; i < this.table[0].length; i++) {
+            this.table[0][i] = { colSpan: 2, content: this.table[0][i] };
+            this.table.options.colAligns[(i - 2) * 2 + 2] = 'center';
+            this.table.options.colAligns[(i - 2) * 2 + 3] = 'center';
+            this.table.options.head[0].colSpan++;
+        }
+
+        // Patch table lines with percentages and colors
+        for (let i = 1; i < this.table.length; i++) {
+            const row = this.table[i];
+            const sortRow = row.slice(1).sort(this.compareElementsHook);
+            const best = sortRow[sortRow.length - 1];
+            const worst = sortRow[0];
+            const comparePercentageValue = this.table[i][1];
+            const tmpRow = [this.table[i][0]];
+            for (let j = 1; j < row.length; j++) {
+                let cellColor;
+                switch (row[j]) {
+                    case best:
+                        cellColor = this.colors.best;
+                        break;
+                    case worst:
+                        cellColor = this.colors.worst;
+                        break;
+                    default:
+                        cellColor = this.colors.neutral;
+                }
+                if (j === 1) {
+                    tmpRow[j] = chalk[cellColor](this.table[i][j]);
+                } else {
+                    let diff = (parseFloat(this.table[i][j].replace(/,/g, '')) / parseFloat(comparePercentageValue.replace(/,/g, ''))) * 100 - 100;
+                    diff = diff > 0 ? `+${diff.toFixed(2)}` : diff.toFixed(2);
+                    tmpRow[(j - 2) * 2 + 2] = chalk[cellColor](this.table[i][j]);
+                    tmpRow[(j - 2) * 2 + 3] = chalk[cellColor](`${diff}%`);
+                }
+            }
+            this.table[i] = tmpRow;
         }
         return this.table.toString();
     }
